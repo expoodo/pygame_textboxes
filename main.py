fdd1095c93ccbb28be5a7dcdc7f332d22b95d7ed
expoodo <foxeproger@gmail.com>
2022-03-15 12:05:59 -0400

    implemented a game loop for testing

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import pygame
 
+SCREEN_WIDTH, SCREEN_HEIGHT = 1000, 800
+running = True
 
+
+pygame.init()
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE, pygame.SCALED)
+pygame.display.set_caption("not sure")
+
+
+while running:
+    events = pygame.event.get()
+
+    screen.fill((0, 0, 0))
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+        elif event.type == pygame.QUIT:
+            running = False
+
+
+
+    clock.tick(120)
+    pygame.display.flip()
