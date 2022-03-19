1f3915e36361390530b20c26a65eba99be221ba3
expoodo <60551350+expoodo@users.noreply.github.com>
2022-03-19 16:23:41 -0400

    Update textsurfaces.py
    messy italic cursor implementation

--- textsurfaces.py
+++ textsurfaces.py
@@ -1,6 +1,7 @@
 import pygame
 import re
 import pyperclip
+import math
 
 
 def separate_words(text: str, return_len_values: bool = False, custom_separators: tuple | list = ("':", ";,.")):
@@ -17,7 +18,7 @@ def separate_words(text: str, return_len_values: bool = False, custom_separators
     # make element have all non-alphanumeric characters as separators if it is None or ''
     custom_separators = ["^\w\W" if s is None or s == "" else s for s in custom_separators]
 
-    split_string = re.findall(fr"\w+(?:(?<=\d)[{custom_separators[1]}]?\d+|[{custom_separators[0]}]?\w*)+\W*", text)
+    split_string = re.findall(fr"\w+(?:(?<=\d)[{custom_separators[1]}]?\d+|[{custom_separators[0]}]?\w*)+\W*|\W+", text)
 
     if return_len_values:
         split_string = [len(element) for element in split_string]
@@ -28,9 +29,7 @@ def separate_words(text: str, return_len_values: bool = False, custom_separators
 class TextLine(pygame.sprite.Sprite):
     """
     Class for managing a single line of text.
-
     cursor position is a number for each character in text, and 0 makes the cursor on the left.
-
     :param text: Text to be shown
     :param font: The font file
     :param size: Size of the font (in pixels)
@@ -54,12 +53,13 @@ class TextLine(pygame.sprite.Sprite):
         self._font = pygame.font.SysFont(self._font_file, self._font_size, self._bold, self._italic)
         self._surface = self._font.render(self._text, self._antialias, self._colour, self._text_background)
         self._rect = self._surface.get_rect()
+        self._true_rect = self._surface.get_rect()
         self._update_requested = False
 
         self.cursor = CursorManager(self._text, self._colour)
         self._left_text, self._right_text = self.cursor.get_split_text()
         self._left_text_size = self._font.size(self._left_text)
-        self._right_text_size = self._font.size(self._right_text)
+        self._right_text_size = self._font.size(self._right_text)  # GOAL: MAKE CURSOR ITALICIZED (TILTED) WHEN FONT IS
         self.is_focused = True
 
         self._request_update()
@@ -72,25 +72,80 @@ class TextLine(pygame.sprite.Sprite):
     def _update_surface(self):
         """update surface and make surface larger to fit in the cursor (only if it is requested)"""
         if self._update_requested:
-            text_surface = self._font.render(self._text, self._antialias, self._colour, self._text_background)
-            background_surface = pygame.Surface((text_surface.get_width() + self.cursor.width,
-                                                 text_surface.get_height()))
+            # prevent surface from updating two times unnecessarily when text is italic and cursor is visible
+            if not self.italic or self.italic and not (self.cursor.visible and self.cursor.enabled):
+                text_surface = self._font.render(self._text, self._antialias, self._colour, self._text_background)
 
-            self._surface = background_surface
-            self._surface.blit(text_surface, (1, 0))
-            self._rect = self._surface.get_rect()
+                if self.italic:  # make background surface bigger to fit in tilted cursor
+                    background_surface = pygame.Surface((text_surface.get_width() + self.cursor.italic_width,
+                                                         text_surface.get_height()))
+                else:
+                    background_surface = pygame.Surface((text_surface.get_width(),
+                                                         text_surface.get_height()))
+
+                self._surface = background_surface
+
+                if self.italic:
+                    self._surface.blit(text_surface, (self.cursor.italic_width, 0))
+                else:
+                    self._surface.blit(text_surface, (0, 0))
+
+                self._rect = self._surface.get_rect()
 
             if self.cursor.visible and self.cursor.enabled:
-                # update left and right substrings around cursor and fill the cursor into the surface
+                # update left and right substrings around cursor and blit cursor surface onto main surface
                 self._left_text, self._right_text = self.cursor.get_split_text()
                 self._left_text_size = self._font.size(self._left_text)
                 self._right_text_size = self._font.size(self._right_text)
 
                 cursor_size = (self.cursor.width, self._surface.get_height())
-                cursor_pos = (self._left_text_size[0], self._rect.top)
-                self._surface.fill(self.cursor.colour, (cursor_pos, cursor_size))
+                cursor_surface = pygame.Surface(cursor_size).convert_alpha()
+
+                if self.cursor.position_is_max():  # make cursor position exactly on the edge of the surface
+                    cursor_pos = (self._left_text_size[0] - self.cursor.width, self._rect.top)
+                else:
+                    cursor_pos = (self._left_text_size[0], self._rect.top)
+
+                cursor_surface.fill(self.cursor.colour)
+
+                if not self.italic:
+                    self._surface.blit(cursor_surface, cursor_pos)
+                else:  # tilt cursor a bit and make surface bigger to make space for it when text is italic
+                    rotated_cursor = pygame.transform.rotate(cursor_surface, self.cursor.angle).convert_alpha()
+                    text_surface = self._font.render(self._text, self._antialias, self._colour, self._text_background)
+
+                    self.cursor.italic_width = rotated_cursor.get_width()
+                    # move cursor more to the right of a space to make the space look bigger
+                    if self.left_text[-1:] == " ":
+                        background_surface = pygame.Surface((text_surface.get_width() + self.cursor.italic_width*2,
+                                                             text_surface.get_height()))
+                        new_rect = rotated_cursor.get_rect(
+                            center=(
+                                (cursor_pos[0] - self.cursor.italic_width / 2 - 1) + self.cursor.italic_width * 2,
+                                cursor_pos[1] + self._rect.height / 2))
+                    elif self.cursor.position_is_min():  # move cursor to the left more when it is at pos 0
+                        background_surface = pygame.Surface((text_surface.get_width() + self.cursor.italic_width * 2,
+                                                             text_surface.get_height()))
+                        new_rect = rotated_cursor.get_rect(
+                            center=(
+                                (cursor_pos[0] - self.cursor.italic_width / 2 - 1) +
+                                self.cursor.italic_width * math.floor(2.5),
+                                cursor_pos[1] + self._rect.height / 2))
+                    else:
+                        background_surface = pygame.Surface((text_surface.get_width() + self.cursor.italic_width,
+                                                             text_surface.get_height()))
+                        new_rect = rotated_cursor.get_rect(
+                            center=(cursor_pos[0] - self.cursor.italic_width / 2 - 1 + self.cursor.italic_width,
+                                    cursor_pos[1] + self._rect.height / 2))
+
+                    self._surface = background_surface
+                    self._surface.blit(text_surface, (self.cursor.italic_width, 0))
+                    self._rect = self._surface.get_rect()
+
+                    self._surface.blit(rotated_cursor, new_rect)
 
             self._update_requested = False
+            print(f"updating surface {pygame.time.get_ticks()}")
 
     def _check_for_cursor_changes(self):
         """check if `self.cursor` properties have been changed"""
@@ -114,6 +169,7 @@ class TextLine(pygame.sprite.Sprite):
                         if len(self.left_text) != 0:
                             if pressed_keys[pygame.K_LCTRL] or pressed_keys[pygame.K_RCTRL]:  # delete words
                                 split_string = separate_words(self.left_text, True)
+
                                 self.left_text = self.left_text[:-split_string[-1]]
                             else:
                                 self.left_text = self.left_text[:-1]
@@ -276,7 +332,6 @@ class TextLine(pygame.sprite.Sprite):
 class CursorManager:
     """
     Class for holding cursor settings and manipulation methods.
-
     :param text: the text that the cursor will be based on
     :param colour: colour of the text
     :param position: position of the cursor corresponding to the position of characters in the text (default value is
@@ -295,6 +350,8 @@ class CursorManager:
         self._property_changed = False
         self._previous_tick_visible = pygame.time.get_ticks()
         self._enabled = True
+        self._angle = -11
+        self._italic_width = 6
 
     def alert_change(self):
         """change _property_changed variable to True when a property is changed"""
@@ -342,6 +399,14 @@ class CursorManager:
                     self.visible = False
         else:
             self._visible = True
+
+    def position_is_max(self):
+        """returns True if the cursor position is at the end of the text"""
+        return True if self.position >= len(self._text) else False
+
+    def position_is_min(self):
+        """returns True if the cursor position is at the start of the text (position 0)"""
+        return True if self.position <= 0 else False
 
     @property
     def width(self):
@@ -419,6 +484,24 @@ class CursorManager:
     @text.setter
     def text(self, value: str):
         self._text = value
+        self.alert_change()
+
+    @property
+    def angle(self):
+        return self._angle
+
+    @angle.setter
+    def angle(self, value: int):
+        self._angle = value
+        self.alert_change()
+
+    @property
+    def italic_width(self):
+        return self._italic_width
+
+    @italic_width.setter
+    def italic_width(self, value):
+        self._italic_width = value
         self.alert_change()
 
 
